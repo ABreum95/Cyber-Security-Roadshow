9a23686530920a6ceb6709fa0da2e3fa6776d5c6
Christian <skou94@gmail.com>
2022-05-24 09:49:32 +0200

    minor db changes

--- php-apache/content/login.php
+++ php-apache/content/login.php
@@ -12,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
    $mypassword = mysqli_real_escape_string($db, $_POST['password']);
 
    // SQL request
-   $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+   $sql = "SELECT id FROM users WHERE username = '$myusername' AND password = '$mypassword'";
    $result = mysqli_query($db, $sql);
    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
    $count = mysqli_num_rows($result);
@@ -23,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       $_SESSION['login_user'] = $myusername;
       header("location: home.php"); // Login and go to home page
    } else {
-      $error = "Your Login Name or Password is invalid";
+      $error = "Your Username or Password is invalid";
    }
 }
 ?>

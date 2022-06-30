812fed6f994879e64560b8210842d23136992c56
Andreas Breum <s176334@student.dtu.dk>
2022-06-30 14:56:59 +0200

    Changed passwords for alice and bob

--- mysql/init.sql
+++ mysql/init.sql
@@ -14,5 +14,5 @@ VALUES
 		"testuser",
 		"9f735e0df9a1ddc702bf0a1a7b83033f9f7153a00c29de82cedadc9957289b05"
 	),
-	(1, "alice", "2bd806c97f0e00af1a1fc3328fa763a9269723c8db8fac4f93af71db186d6e90"),
-	(2, "bob", "81b637d8fcd2c6da6359e6963113a1170de795e4b725b84d1e0b4cfd9ec58ce9");
+	(1, "alice", "51294ec24ab55726f4805beaebd65e234e795760432a03e03de86c7e31567f4e"), -- vsknj2sdiX
+	(2, "bob", "acf322c8a528719cba9bf4fcf206a04ddd0784668fb0f7b2f93ec78b8983d051"); -- Spongebob06

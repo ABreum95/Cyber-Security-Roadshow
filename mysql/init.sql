ecacc3cd64bef5186ccdfe757d6dd628cfd80588
Christian <skou94@gmail.com>
2022-07-14 13:09:30 +0200

    password changes

--- mysql/init.sql
+++ mysql/init.sql
@@ -9,5 +9,5 @@ CREATE TABLE users (
 INSERT INTO
 	users
 VALUES
-	(0, "alice", "51294ec24ab55726f4805beaebd65e234e795760432a03e03de86c7e31567f4e"), -- vsknj2sdiX
-	(1, "bob", "f0e2e750791171b0391b682ec35835bd6a5c3f7c8d1d0191451ec77b4d75f240"); -- spongebob
+	(0, "alice", "51294ec24ab55726f4805beaebd65e234e795760432a03e03de86c7e31567f4e"),
+	(1, "bob", "f0e2e750791171b0391b682ec35835bd6a5c3f7c8d1d0191451ec77b4d75f240");
